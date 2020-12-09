ae1378e0db90e2d5458c6fe45d11023c5851143e
jakablevente <jakablevente29@gmail.com>
2020-12-09 16:43:13 +0100

    new category with modal

--- src/main/java/com/ims/controller/ProductController.java
+++ src/main/java/com/ims/controller/ProductController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -129,22 +133,21 @@ public class ProductController {
 		List<Category> listCategory = categoryService.listAll();
 		model.addAttribute("listCategory", listCategory);
 		
-		return "category";
-	}
-	
-	@RequestMapping("/newcategory")
-	public String showNewCategoryPage(Model model) {
-		
 		Category category = new Category();
 		model.addAttribute("category", category);
 		
-		return "new_category";
+		return "category";
 	}
 	
-	@RequestMapping(value="/save_category",method = RequestMethod.POST)
-	public String saveCategory(@ModelAttribute ("category") Category category) {
+	
+
+	
+	
+	@RequestMapping(value="/addcategory",method = RequestMethod.POST)
+	public String addCategory(@ModelAttribute ("category") Category category) {
 		
 		categoryService.save(category);
+		
 		return "redirect:/";
 	}
 	
