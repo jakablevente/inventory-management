c28eeb0db56594a3902a8d7340ad7591796077fd
jakablevente <jakablevente29@gmail.com>
2020-12-18 22:25:43 +0100

    layout added

--- src/main/java/com/ims/controller/ProductController.java
+++ src/main/java/com/ims/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import com.ims.model.Product;
@@ -19,46 +19,40 @@ import com.ims.service.ProductService;
 public class ProductController {
 	
 	@Autowired
-	private ProductService service;
+	private ProductService productService;
 	
 	
 	@RequestMapping("/product")
 	public String viewProductPage(Model model) {
 		
-		List<Product> listProducts = service.listAll();
+		List<Product> listProducts = productService.listAll();
 		model.addAttribute("listProducts", listProducts);
+		
+		Product product = new Product();
+		model.addAttribute("product", product);
 		
 		return "product";
 	}
 	
-	@RequestMapping("/new")
-	public String showNewProductPage(Model model) {
-		Product product = new Product();
-		model.addAttribute("product", product);
-		
-		return "new_product";
-	}
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveProduct(@ModelAttribute("product") Product product) {
-		service.save(product);
+		productService.save(product);
 		
 		return "redirect:/";
 	}
 	
-	@RequestMapping("/edit/{id}")
-	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
-		ModelAndView mav = new ModelAndView("edit_product");
-		Product product = service.get(id);
-		mav.addObject("product", product);
-		
-		return mav;
-	}
 	
 	@RequestMapping("/delete/{id}")
 	public String deleteProduct(@PathVariable(name = "id") int id) {
-		service.delete(id);
+		productService.delete(id);
 		return "redirect:/";		
+	}
+	
+	@RequestMapping("/findProduct")
+	@ResponseBody
+	public Product findProduct(Integer id) {
+		return productService.get(id);
 	}
 	
 	
